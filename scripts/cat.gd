9bcd8bd4cbb77f49bfbfde889146d1c7403cd62f
licomnaklavy <andrey.vtorov.2@gmail.com>
2024-12-24 14:13:46 +0300

    баги исправлены

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -1,39 +1,40 @@
 extends Area2D
 
 
-var current_instrument
-var is_playing
-var start_position
 var cat_sprite
-var in_area
+var in_area: bool
+var current_instrument
 
 
 func _ready() -> void:
 	cat_sprite = self.find_child("cat_sprite")
-	is_playing = false
-
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if Input.is_action_just_pressed("left_key") and is_playing: # котик возвращает инструмент
-		cat_sprite.animation = "sleep"
-		current_instrument.visible = true
-		current_instrument.position = start_position
-		is_playing = false
-
-func _on_area_entered(area: Area2D) -> void: # котику приходит инструмент
-	if !is_playing: # сохраняет <настоящий> инструмент
-		cat_sprite.modulate = "73af00" # ЗАМЕНИТЬ ПОДСВЕТКУ
-		current_instrument = area
-		start_position = $"../empty_sprites".find_child(current_instrument.name).position
-		in_area = true
-
-func _on_area_exited(area: Area2D) -> void: # от котика уходит инструмент
-	cat_sprite.modulate = "ffffff" # ЗАМЕНИТЬ ПОДСВЕТКУ
 	in_area = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_released("left_key") and in_area: # котик получил инструмент
-		if !is_playing:
+func _on_area_entered(area: Area2D) -> void:
+	if !get_meta("is_playing"):
+		in_area = true
+		current_instrument = area
+
+func _on_area_exited(area: Area2D) -> void:
+	in_area = false
+	cat_sprite.modulate = "ffffff" # ЗАМЕНИТЬ ПОДСВЕТКУ
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("left_key"):
+		if get_meta("is_playing"):
+			cat_sprite.animation = "sleep"
+			current_instrument.visible = true
+			set_meta("is_playing", false)
+	
+	if Input.is_action_just_released("left_key"):
+		if !get_meta("is_playing") and in_area:
+			current_instrument.visible = false
 			cat_sprite.animation = "playing_" + current_instrument.name
-			is_playing = true
-			in_area = false
-			cat_sprite.modulate = "ffffff" # ЗАМЕНИТЬ ПОДСВЕТКУ
+			set_meta("is_playing", true)
+
+func _on_mouse_entered() -> void:
+	if in_area:
+		cat_sprite.modulate = "73af00" # ЗАМЕНИТЬ ПОДСВЕТКУ
+
+func _on_mouse_exited() -> void:
+	cat_sprite.modulate = "ffffff" # ЗАМЕНИТЬ ПОДСВЕТКУ
--- scripts/instrument.gd
+++ scripts/instrument.gd
@@ -1,19 +1,16 @@
 extends Area2D
 
-var start_position # начальная позиция инструменты
-var in_area = false # у котика
-var dragging = false # перетаскивание
-var mousepos
+
+var start_position: Vector2
+var mousepos: Vector2
+var dragging: bool
 
 var cat
-var cat_sprite
-var cat_counter
 
 
 # процессы перетаскивания
 func _ready() -> void:
 	start_position = self.position
-	cat_counter = 0
 
 func _process(delta):
 	if dragging:
@@ -21,30 +18,21 @@ func _process(delta):
 		self.position = mousepos
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
-		if Input.is_action_just_pressed("left_key"):
-			dragging = true
-		elif Input.is_action_just_released("left_key") and dragging:
-			dragging = false
+	if Input.is_action_just_pressed("left_key"):
+		dragging = true
+	if Input.is_action_just_released("left_key") and dragging:
+		dragging = false
 
 
 # столкновение с котиком
-func _on_area_entered(area: Area2D) -> void: # инструмент пришёл к котику
-	cat_counter+=1
+func _on_area_entered(area: Area2D) -> void:
 	cat = area
-	cat_sprite = cat.find_child("cat_sprite")
-	if cat_sprite.animation == "sleep" and cat_counter <= 1:
-		in_area = true
+	set_meta("cat_counter", get_meta("cat_counter") + 1)
 
-func _on_area_exited(area: Area2D) -> void: # инструмент ушёл от котика
-	cat_counter-=1
-	in_area = false
+func _on_area_exited(area: Area2D) -> void:
+	set_meta("cat_counter", get_meta("cat_counter") - 1)
 
 func _physics_process(delta: float) -> void:
-	print(cat_counter)
-	if Input.is_action_just_released("left_key"): # отдать инстурмент котику
-		if in_area:
-			in_area = false
-			self.visible = false
-		else:
-			self.position = start_position # возврат инструмента при уходе
+	print(get_meta("cat_counter"))
+	if Input.is_action_just_released("left_key"):
+		self.position = start_position
